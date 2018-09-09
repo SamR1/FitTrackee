124fe88ec18e6ed1b2f664c7f2d09529ee672aa1
Sam <sam@samr1.net>
2018-09-09 10:48:32 +0200

    add install-db in Makefile

--- fittrackee_api/db/create.sql
+++ fittrackee_api/db/create.sql
@@ -1,5 +1,7 @@
 DROP DATABASE IF EXISTS fittrackee;
 DROP DATABASE IF EXISTS fittrackee_test;
+DROP USER fittrackee;
+
 CREATE DATABASE fittrackee;
 CREATE DATABASE fittrackee_test;
 CREATE USER fittrackee WITH PASSWORD 'fittrackee';
