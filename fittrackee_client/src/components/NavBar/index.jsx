18df5b994adb34324c3c0143b8da1a67e622bea7
Sam <sam@samr1.net>
2019-01-09 12:07:57 +0100

    refactor - use of PureComponents to avoid useless re-rendering

--- fittrackee_client/src/components/NavBar/index.jsx
+++ fittrackee_client/src/components/NavBar/index.jsx
@@ -4,136 +4,141 @@ import { Link } from 'react-router-dom'
 
 import { apiUrl } from '../../utils'
 
-
-function NavBar(props) {
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <span className="navbar-brand">FitTrackee</span>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to={{
-                    pathname: '/',
-                  }}
-                >
-                  Dashboard
-                </Link>
-              </li>
-              {props.user.isAuthenticated && (
+class NavBar extends React.PureComponent {
+  render() {
+    const { user } = this.props
+    return (
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <span className="navbar-brand">FitTrackee</span>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
                   <Link
                     className="nav-link"
                     to={{
-                      pathname: '/activities/history',
+                      pathname: '/',
                     }}
                   >
-                    Workouts
+                    Dashboard
                   </Link>
                 </li>
-              )}
-              {props.user.isAuthenticated && (
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to={{
-                      pathname: '/activities/add',
-                    }}
-                  >
-                    <strong>Add workout</strong>
-                  </Link>
-                </li>
-              )}
-              {/* {props.user.admin && ( */}
-                {/* <li className="nav-item"> */}
-                  {/* <Link */}
-                    {/* className="nav-link" */}
-                    {/* to={{ */}
-                      {/* pathname: '/admin', */}
-                    {/* }} */}
-                  {/* > */}
-                    {/* Admin */}
-                  {/* </Link> */}
-                {/* </li> */}
-              {/* )} */}
-            </ul>
-            <ul className="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-              {!props.user.isAuthenticated && (
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to={{
-                      pathname: '/register',
-                    }}
-                  >
-                    Register
-                  </Link>
-                </li>
-              )}
-              {!props.user.isAuthenticated && (
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to={{
-                      pathname: '/login',
-                    }}
-                  >
-                    Login
-                  </Link>
-                </li>
-              )}
-              {props.user.picture === true && (
-                <img
-                  alt="Avatar"
-                  src={`${apiUrl}users/${props.user.id}/picture` +
-                  `?${Date.now()}`}
-                  className="img-fluid App-nav-profile-img"
-                />
-              )}
-              {props.user.isAuthenticated && (
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to={{
-                      pathname: '/profile',
-                    }}
-                  >
-                    {props.user.username}
-                  </Link>
-                </li>
-              )}
-              {props.user.isAuthenticated && (
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to={{
-                      pathname: '/logout',
-                    }}
-                  >
-                    Logout
-                  </Link>
-                </li>
-              )}
-            </ul>
+                {user.isAuthenticated && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to={{
+                        pathname: '/activities/history',
+                      }}
+                    >
+                      Workouts
+                    </Link>
+                  </li>
+                )}
+                {user.isAuthenticated && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to={{
+                        pathname: '/activities/add',
+                      }}
+                    >
+                      <strong>Add workout</strong>
+                    </Link>
+                  </li>
+                )}
+                {/* {user.admin && ( */}
+                  {/* <li className="nav-item"> */}
+                    {/* <Link */}
+                      {/* className="nav-link" */}
+                      {/* to={{ */}
+                        {/* pathname: '/admin', */}
+                      {/* }} */}
+                    {/* > */}
+                      {/* Admin */}
+                    {/* </Link> */}
+                  {/* </li> */}
+                {/* )} */}
+              </ul>
+              <ul className="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+                {!user.isAuthenticated && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to={{
+                        pathname: '/register',
+                      }}
+                    >
+                      Register
+                    </Link>
+                  </li>
+                )}
+                {!user.isAuthenticated && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to={{
+                        pathname: '/login',
+                      }}
+                    >
+                      Login
+                    </Link>
+                  </li>
+                )}
+                {user.picture === true && (
+                  <img
+                    alt="Avatar"
+                    src={`${apiUrl}users/${user.id}/picture` +
+                    `?${Date.now()}`}
+                    className="img-fluid App-nav-profile-img"
+                  />
+                )}
+                {user.isAuthenticated && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to={{
+                        pathname: '/profile',
+                      }}
+                    >
+                      {user.username}
+                    </Link>
+                  </li>
+                )}
+                {user.isAuthenticated && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to={{
+                        pathname: '/logout',
+                      }}
+                    >
+                      Logout
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
-  )
+        </nav>
+      </header>
+    )
+  }
 }
 
 export default connect(
