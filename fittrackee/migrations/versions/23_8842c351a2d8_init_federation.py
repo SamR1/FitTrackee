05d10153d9d887e02b760c276d3159b1577083fb
Sam <sam@samr1.net>
2021-02-03 20:17:28 +0100

    API - init domain and actors creation in migration

--- fittrackee/migrations/versions/23_8842c351a2d8_init_federation.py
+++ fittrackee/migrations/versions/23_8842c351a2d8_init_federation.py
@@ -5,8 +5,13 @@ Revises: 4e8597c50064
 Create Date: 2021-01-10 16:02:43.811023
 
 """
-from alembic import op
+import os
+from datetime import datetime
+
 import sqlalchemy as sa
+from alembic import op
+
+from fittrackee.federation.utils import get_ap_url
 
 
 # revision identifiers, used by Alembic.
@@ -26,7 +31,7 @@ def upgrade():
     op.execute('UPDATE app_config SET federation_enabled = false')
     op.alter_column('app_config', 'federation_enabled', nullable=False)
 
-    op.create_table('domains',
+    domain_table = op.create_table('domains',
         sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
         sa.Column('name', sa.String(length=1000), nullable=False),
         sa.Column('created_at', sa.DateTime(), nullable=False),
@@ -35,7 +40,14 @@ def upgrade():
         sa.UniqueConstraint('name'),
     )
 
-    op.create_table('actors',
+    domain = os.environ['UI_URL']
+    created_at = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    op.execute(
+        "INSERT INTO domains (name, created_at, is_allowed)"
+        f"VALUES ('{domain}', '{created_at}'::timestamp, True)"
+    )
+
+    actors_table = op.create_table('actors',
         sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
         sa.Column('activitypub_id', sa.String(length=255), nullable=False),
         sa.Column('domain_id', sa.Integer(), nullable=False),
@@ -60,6 +72,39 @@ def upgrade():
     op.add_column('users', sa.Column('actor_id', sa.Integer(), nullable=True))
     op.create_unique_constraint('users_actor_id_key', 'users', ['actor_id'])
     op.create_foreign_key('users_actor_id_fkey', 'users', 'actors', ['actor_id'], ['id'])
+
+    # create local actors
+    user_helper = sa.Table(
+        'users',
+        sa.MetaData(),
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('username', sa.String(length=20), nullable=False),
+    )
+    connection = op.get_bind()
+    domain = connection.execute(domain_table.select()).fetchone()
+    for user in connection.execute(user_helper.select()):
+        created_at = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+        op.execute(
+            "INSERT INTO actors ("
+            "activitypub_id, domain_id, preferred_username, followers_url, "
+            "following_url, inbox_url, outbox_url, shared_inbox_url, "
+            "created_at, manually_approves_followers) "
+            "VALUES ("
+            f"'{get_ap_url(user.username, 'user_url')}', "
+            f"{domain.id}, '{user.username}', "
+            f"'{get_ap_url(user.username, 'followers')}', "
+            f"'{get_ap_url(user.username, 'following')}', "
+            f"'{get_ap_url(user.username, 'inbox')}', "
+            f"'{get_ap_url(user.username, 'outbox')}', "
+            f"'{get_ap_url(user.username, 'shared_inbox')}', "
+            f"'{created_at}'::timestamp, {True}) RETURNING id"
+        )
+        actor = connection.execute(actors_table.select().where(
+            actors_table.c.preferred_username == user.username)).fetchone()
+        op.execute(
+            f'UPDATE users SET actor_id = {actor.id} WHERE users.id = {user.id}'
+        )
+
 
     op.create_table('follow_requests',
         sa.Column('follower_actor_id', sa.Integer(), nullable=False),
