433f6bfd52a2c98808c4fea9eda1d1375a199d2f
Sam <sam@samr1.net>
2021-07-25 13:03:34 +0200

    Client - add navbar component

--- fittrackee_client/src/App.vue
+++ fittrackee_client/src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <NavBar />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    NavBar,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
---
+++ fittrackee_client/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+
+<style scoped></style>
