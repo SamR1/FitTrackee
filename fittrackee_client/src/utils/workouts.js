bf93c7f469e710e0f4e060846a8facca2d6aa477
Sam <sam@samr1.net>
2021-09-01 12:55:12 +0200

    Client - update sports color

--- fittrackee_client/src/utils/workouts.js
+++ fittrackee_client/src/utils/workouts.js
@@ -4,17 +4,17 @@ import togeojson from '@mapbox/togeojson'
 import { getDateWithTZ } from './index'
 
 export const workoutColors = [
-  '#F94144',
-  '#F3722C',
-  '#F8961E',
-  '#F9844A',
-  '#F9C74F',
-  '#90BE6D',
-  '#43AA8B',
-  '#4D908E',
-  '#577590',
-  '#277DA1',
-  '#272DA1',
+  '#55a8a3', // Cycling (sport)
+  '#98C3A9', // Cycling (transport)
+  '#D0838A', // Hiking
+  '#ECC77E', // Mountain bike
+  '#926692', // Running
+  '#929292', // Walking
+  '#f7af88', // Mountain Biking (Electric)
+  '#fbc2a6', // Trail
+  '#67a4bd', // Skiing (Alpine)
+  '#9498d0', // Skiing (Cross Country)
+  '#fad783', // Rowing
 ]
 
 export const recordsLabels = [
