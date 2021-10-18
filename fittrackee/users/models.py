1551c45afdc5d5d492930c270ad34b2e8e954f64
Fmstrat <nospam@nowsci.com>
2021-10-18 14:26:25 -0400

    Added total ascent to statistics

--- fittrackee/users/models.py
+++ fittrackee/users/models.py
@@ -115,7 +115,7 @@ class User(BaseModel):
             )
             total = (
                 db.session.query(
-                    func.sum(Workout.distance), func.sum(Workout.duration)
+                    func.sum(Workout.distance), func.sum(Workout.duration), func.sum(Workout.ascent)
                 )
                 .filter(Workout.user_id == self.id)
                 .first()
@@ -141,4 +141,5 @@ class User(BaseModel):
             ],
             'total_distance': float(total[0]),
             'total_duration': str(total[1]),
+            'total_ascent': str(total[2]),
         }
