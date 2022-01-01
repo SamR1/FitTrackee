a2ea4c212d9c8f2cea477ea365ca3739997eb1da
Sam <sam@samr1.net>
2022-01-01 22:47:01 +0100

    bump version to 0.5.4

--- fittrackee/__init__.py
+++ fittrackee/__init__.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from fittrackee.emails.email import Email
 
-VERSION = __version__ = '0.5.3'
+VERSION = __version__ = '0.5.4'
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
