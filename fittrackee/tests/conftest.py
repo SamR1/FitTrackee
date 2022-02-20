b0de9370613e0866db57d81dcaf23a41bb1c8469
Sam <sam@samr1.net>
2022-02-20 14:12:22 +0100

    API - minor test fix after rebase

--- fittrackee/tests/conftest.py
+++ fittrackee/tests/conftest.py
@@ -2,8 +2,6 @@ import os
 
 os.environ['FLASK_ENV'] = 'testing'
 os.environ['APP_SETTINGS'] = 'fittrackee.config.TestingConfig'
-os.environ['UI_URL'] = 'https://0.0.0.0:5000'
-os.environ['SENDER_EMAIL'] = 'fittrackee@example.com'
 # to avoid resetting dev database during tests
 os.environ['DATABASE_URL'] = os.environ['DATABASE_TEST_URL']
 
