bc15421b3968eacdd7671a47323a19908f24cb44
Sam <sam@samr1.net>
2023-07-14 17:43:39 +0200

    Tests - add missing e2e test (add a workout w/ gpx)

--- e2e/test_workouts.py
+++ e2e/test_workouts.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select, WebDriverWait
@@ -42,3 +44,28 @@ class TestWorkout:
         assert 'Distance: 10 km' in workout_details
         assert 'Average Speed: 10 km/h' in workout_details
         assert 'Max. Speed: 10 km/h' in workout_details
+
+    def test_user_can_add_workout_with_gpx(self, selenium):
+        register_valid_user(selenium)
+        app_menu = selenium.find_element(By.CLASS_NAME, 'nav-items-app-menu')
+        add_workout_link = app_menu.find_elements(By.CLASS_NAME, 'nav-item')[3]
+
+        add_workout_link.click()
+        selenium.implicitly_wait(1)
+
+        select = Select(selenium.find_element(By.ID, 'sport'))
+        select.select_by_index(1)
+        gpx_input = selenium.find_element(By.XPATH, "//input[@type='file']")
+        gpx_input.send_keys(os.getcwd() + "/e2e/test.gpx")
+
+        confirm_button = selenium.find_elements(By.CLASS_NAME, 'confirm')[-1]
+        confirm_button.click()
+
+        WebDriverWait(selenium, 30).until(
+            EC.url_changes(f"{TEST_URL}/workouts/add")
+        )
+
+        workout_details = selenium.find_element(By.ID, 'workout-info').text
+        assert 'Duration: 0:04:10' in workout_details
+        assert 'Distance: 0.32 km' in workout_details
+        assert 'Average Speed: 4.61 km/h' in workout_details
