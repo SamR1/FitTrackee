0c4b8f1b98033dcfa870cc80d28647aea4658a6c
Sam <sam@samr1.net>
2023-11-19 11:58:45 +0100

    Client - fix chart display on mobile

--- fittrackee_client/src/components/Common/StatsChart/Chart.vue
+++ fittrackee_client/src/components/Common/StatsChart/Chart.vue
@@ -203,15 +203,22 @@
   @import '~@/scss/vars.scss';
 
   .bar-chart {
+    position: relative;
     min-height: 400px;
+    width: 100%;
     &.minimal {
       min-height: 300px;
     }
 
     @media screen and (max-width: $small-limit) {
+      min-height: 268px;
       &.minimal {
         min-height: 290px;
       }
+    }
+
+    @media screen and (max-width: 420px) {
+      width: calc(100vw - 95px);
     }
   }
 </style>
