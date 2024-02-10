da97c5c85a35e6377c6ecf023b44c45f48648c85
Sam <sam@samr1.net>
2024-02-10 08:56:53 +0100

    API - init Basque (eu) translation files for emails

--- fittrackee/config.py
+++ fittrackee/config.py
@@ -67,6 +67,7 @@ class BaseConfig:
         'es',
         'gl',
         'pl',
+        # 'eu',  # disabled for now
     ]
     OAUTH2_TOKEN_EXPIRES_IN = {
         'authorization_code': 864000,  # 10 days
