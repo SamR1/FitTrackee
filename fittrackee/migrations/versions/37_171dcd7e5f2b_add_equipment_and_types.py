109527e7120e22a5cb64e3181b07257348bd6a28
Sam <sam@samr1.net>
2024-03-02 14:40:31 +0100

    API - update migration after merge

--- fittrackee/migrations/versions/37_171dcd7e5f2b_add_equipment_and_types.py
+++ fittrackee/migrations/versions/37_171dcd7e5f2b_add_equipment_and_types.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '171dcd7e5f2b'
-down_revision = '24eb097614e4'
+down_revision = '4d51a4ca8001'
 branch_labels = None
 depends_on = None
 
