642ecc62a2d0742541b2f6502f155778af1ade00
Sam <sam@samr1.net>
2024-12-04 11:33:05 +0100

    Client - fix tabs in user profile

--- fittrackee_client/src/components/User/ProfileDisplay/index.vue
+++ fittrackee_client/src/components/User/ProfileDisplay/index.vue
@@ -2,18 +2,22 @@
   <div id="user-profile">
     <UserHeader :user="user" />
     <div class="box">
-      <UserProfileTabs :tabs="tabs" :selectedTab="tab" :edition="false" />
+      <UserProfileTabs :tabs="tabGroup1" :selectedTab="tab" :edition="false" />
+      <UserProfileTabs :tabs="tabGroup2" :selectedTab="tab" :edition="false" />
       <router-view :user="user"></router-view>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { toRefs } from 'vue'
+  import { computed, toRefs } from 'vue'
+  import type { ComputedRef } from 'vue'
 
   import UserHeader from '@/components/User/ProfileDisplay/UserHeader.vue'
   import UserProfileTabs from '@/components/User/UserProfileTabs.vue'
+  import { AUTH_USER_STORE } from '@/store/constants'
   import type { IUserProfile } from '@/types/user'
+  import { useStore } from '@/use/useStore'
 
   interface Props {
     user: IUserProfile
@@ -22,14 +26,19 @@
   const props = defineProps<Props>()
   const { user, tab } = toRefs(props)
 
-  const tabs = [
-    'PROFILE',
-    'PREFERENCES',
-    'SPORTS',
-    'EQUIPMENTS',
-    'APPS',
-    'MODERATION',
-  ]
+  const store = useStore()
+
+  const isSuspended: ComputedRef<boolean> = computed(
+    () => store.getters[AUTH_USER_STORE.GETTERS.IS_SUSPENDED]
+  )
+  const tabGroup1 = computed(() =>
+    isSuspended.value
+      ? ['PROFILE', 'PREFERENCES', 'SPORTS', 'EQUIPMENTS', 'APPS', 'MODERATION']
+      : ['PROFILE', 'PREFERENCES', 'SPORTS', 'EQUIPMENTS', 'APPS']
+  )
+  const tabGroup2 = computed(() =>
+    isSuspended.value ? [] : ['FOLLOW-REQUESTS', 'BLOCKED-USERS', 'MODERATION']
+  )
 </script>
 
 <style lang="scss" scoped>
