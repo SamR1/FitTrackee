35d19d3604a2bb9508afebc6059529a395f05cf1
Sam <sam@samr1.net>
2024-12-08 10:29:51 +0100

    Client - add info about private notes on workout creation

--- fittrackee_client/src/components/Workout/WorkoutEdition.vue
+++ fittrackee_client/src/components/Workout/WorkoutEdition.vue
@@ -343,13 +343,19 @@
                 </div>
               </div>
               <div class="form-item" v-if="isCreation">
-                <label for="notes"> {{ $t('workouts.NOTES') }}: </label>
+                <label for="notes"> {{ $t('workouts.PRIVATE_NOTES') }}: </label>
                 <CustomTextArea
                   name="notes"
                   :input="workoutForm.notes"
                   :disabled="loading"
                   @updateValue="updateNotes"
                 />
+                <div class="field-help" v-if="isCreation">
+                  <span class="info-box">
+                    <i class="fa fa-info-circle" aria-hidden="true" />
+                    {{ $t('workouts.PRIVATE_NOTES_FIELD_HELP') }}
+                  </span>
+                </div>
               </div>
             </div>
             <ErrorMessage :message="errorMessages" v-if="errorMessages" />
