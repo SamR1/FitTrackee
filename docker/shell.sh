feecb9e8cbc0611b49606374a54c479c45793aff
sam@samr1.net <sam@samr1.net>
2024-12-26 10:02:06 +0100

    Docker - update files for dev

--- docker/shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd /usr/src/app
-
-source .env
-
-/bin/bash
--- docker/test-e2e.sh
+++ docker/test-e2e.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-cd /usr/src/app
-
-source .env
 
 export TEST_APP_URL=http://$(hostname --ip-address):5000
 pytest e2e --driver Remote --capability browserName firefox --selenium-host selenium --selenium-port 4444 $*
