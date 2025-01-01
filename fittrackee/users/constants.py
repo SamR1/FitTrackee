6b6abd744f683840f56532802990a019b4710e09
Sam <sam@samr1.net>
2025-01-01 16:02:04 +0100

    API - minor refacto

--- fittrackee/users/constants.py
+++ fittrackee/users/constants.py
@@ -1,3 +1,35 @@
 USER_DATE_FORMAT = 'MM/dd/yyyy'
 USER_LANGUAGE = 'en'
 USER_TIMEZONE = 'Europe/Paris'
+USER_LINK_TEMPLATE = (
+    '<a href="{profile_url}" target="_blank" rel="noopener noreferrer">'
+    '{username}</a>'
+)
+
+ADMINISTRATOR_NOTIFICATION_TYPES = [
+    'account_creation',
+]
+MODERATOR_NOTIFICATION_TYPES = [
+    'report',
+    'suspension_appeal',
+    'user_warning_appeal',
+]
+NOTIFICATION_TYPES = (
+    ADMINISTRATOR_NOTIFICATION_TYPES
+    + MODERATOR_NOTIFICATION_TYPES
+    + [
+        'comment_like',
+        'comment_suspension',
+        'comment_unsuspension',
+        'follow',
+        'follow_request',
+        'follow_request_approved',
+        'mention',
+        'user_warning',
+        'user_warning_lifting',
+        'workout_comment',
+        'workout_like',
+        'workout_suspension',
+        'workout_unsuspension',
+    ]
+)
--- fittrackee/users/models.py
+++ fittrackee/users/models.py
@@ -24,6 +24,7 @@ from fittrackee.utils import encode_uuid
 from fittrackee.visibility_levels import VisibilityLevel
 from fittrackee.workouts.models import Workout
 
+from .constants import NOTIFICATION_TYPES, USER_LINK_TEMPLATE
 from .exceptions import (
     BlockUserException,
     FollowRequestAlreadyProcessedError,
@@ -41,41 +42,6 @@ from .utils.token import decode_user_token, get_user_token
 
 if TYPE_CHECKING:
     from fittrackee.reports.models import ReportAction
-
-USER_LINK_TEMPLATE = (
-    '<a href="{profile_url}" target="_blank" rel="noopener noreferrer">'
-    '{username}</a>'
-)
-
-ADMINISTRATOR_NOTIFICATION_TYPES = [
-    'account_creation',
-]
-
-MODERATOR_NOTIFICATION_TYPES = [
-    'report',
-    'suspension_appeal',
-    'user_warning_appeal',
-]
-
-NOTIFICATION_TYPES = (
-    ADMINISTRATOR_NOTIFICATION_TYPES
-    + MODERATOR_NOTIFICATION_TYPES
-    + [
-        'comment_like',
-        'comment_suspension',
-        'comment_unsuspension',
-        'follow',
-        'follow_request',
-        'follow_request_approved',
-        'mention',
-        'user_warning',
-        'user_warning_lifting',
-        'workout_comment',
-        'workout_like',
-        'workout_suspension',
-        'workout_unsuspension',
-    ]
-)
 
 
 class FollowRequest(BaseModel):
