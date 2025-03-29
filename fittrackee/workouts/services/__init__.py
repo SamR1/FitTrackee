99970748c37161671a01d7d8a22716eb227bb01f
Sam <sam@samr1.net>
2025-03-29 16:54:54 +0100

    API - update task during archive import & delete temp file after import

--- fittrackee/workouts/services/__init__.py
+++ fittrackee/workouts/services/__init__.py
@@ -2,6 +2,7 @@ from .workout_creation_service import WorkoutCreationService
 from .workout_from_file import WorkoutGpxCreationService
 from .workout_update_service import WorkoutUpdateService
 from .workouts_from_file_creation_service import (
+    WorkoutsFromArchiveCreationAsyncService,
     WorkoutsFromFileCreationService,
 )
 
@@ -9,5 +10,6 @@ __all__ = [
     "WorkoutCreationService",
     "WorkoutGpxCreationService",
     "WorkoutUpdateService",
+    "WorkoutsFromArchiveCreationAsyncService",
     "WorkoutsFromFileCreationService",
 ]
