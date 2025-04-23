37f84021802068aaa64c56b9f5dee198c9e20a54
Sam <sam@samr1.net>
2025-04-23 13:59:05 +0200

    CLI - add command to process a given queued task for workouts upload

--- fittrackee/tests/workouts/test_workouts_commands.py
+++ fittrackee/tests/workouts/test_workouts_commands.py
@@ -13,7 +13,7 @@ from fittrackee.workouts.services.workouts_from_file_creation_service import (
     WorkoutsFromArchiveCreationAsyncService,
 )
 
-from ..mixins import UserTaskMixin
+from ..mixins import RandomMixin, UserTaskMixin
 
 if TYPE_CHECKING:
     from _pytest.logging import LogCaptureFixture
@@ -23,7 +23,7 @@ if TYPE_CHECKING:
     from fittrackee.workouts.models import Sport
 
 
-class TestCliWorkoutsArchiveUpload(UserTaskMixin):
+class TestCliWorkoutsArchiveUploads(UserTaskMixin):
     def test_it_displays_0_when_no_archives_to_process(
         self, app: "Flask", caplog: "LogCaptureFixture", user_1: "User"
     ) -> None:
@@ -36,7 +36,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
             cli,
             [
                 "workouts",
-                "archive_upload",
+                "archive_uploads",
                 "--max",
                 "1",
             ],
@@ -55,7 +55,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
             cli,
             [
                 "workouts",
-                "archive_upload",
+                "archive_uploads",
                 "--max",
                 "1",
             ],
@@ -64,7 +64,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         assert result.exit_code == 1
         assert isinstance(result.exception, WorkoutException)
 
-    def test_it_calls_process_workouts_archives_upload(
+    def test_it_calls_process_workouts_archives_uploads(
         self,
         app: "Flask",
         user_1: "User",
@@ -81,20 +81,20 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         max_archives = 3
 
         with patch(
-            "fittrackee.workouts.commands.process_workouts_archives_upload",
+            "fittrackee.workouts.commands.process_workouts_archives_uploads",
             return_value=max_archives,
-        ) as process_workouts_archives_upload_mock:
+        ) as process_workouts_archives_uploads_mock:
             runner.invoke(
                 cli,
                 [
                     "workouts",
-                    "archive_upload",
+                    "archive_uploads",
                     "--max",
                     str(max_archives),
                 ],
             )
 
-        process_workouts_archives_upload_mock.assert_called_once_with(
+        process_workouts_archives_uploads_mock.assert_called_once_with(
             max_archives, logger
         )
 
@@ -128,7 +128,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
                 cli,
                 [
                     "workouts",
-                    "archive_upload",
+                    "archive_uploads",
                     "--max",
                     "1",
                 ],
@@ -137,7 +137,8 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         assert result.exit_code == 0
         assert (
             caplog.records[0].message
-            == "Processing task '1' (files: 2, size: 0 Bytes)"
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 2, size: 0 Bytes)"
         )
         assert caplog.records[1].message == " > errored files: 1"
         assert caplog.records[2].message == " > done."
@@ -151,7 +152,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         sport_1_cycling: "Sport",
     ) -> None:
         # archive file does not exist
-        self.create_workouts_upload_task(
+        upload_task = self.create_workouts_upload_task(
             user_1,
             workouts_data={"sport_id": sport_1_cycling.id},
             files_to_process=["file_1.gpx", "file_2.gpx"],
@@ -164,7 +165,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
             cli,
             [
                 "workouts",
-                "archive_upload",
+                "archive_uploads",
                 "--max",
                 "1",
             ],
@@ -174,7 +175,8 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
 
         assert (
             caplog.records[0].message
-            == "Processing task '1' (files: 2, size: 0 Bytes)"
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 2, size: 0 Bytes)"
         )
         assert (
             caplog.records[1].message
@@ -190,7 +192,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         user_1: "User",
         sport_1_cycling: "Sport",
     ) -> None:
-        self.create_workouts_upload_task(
+        upload_task = self.create_workouts_upload_task(
             user_1,
             workouts_data={"sport_id": sport_1_cycling.id},
             files_to_process=["example.gpx"],
@@ -208,7 +210,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
                 cli,
                 [
                     "workouts",
-                    "archive_upload",
+                    "archive_uploads",
                     "--max",
                     "1",
                 ],
@@ -217,7 +219,8 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         assert result.exit_code == 0
         assert (
             caplog.records[0].message
-            == "Processing task '1' (files: 1, size: 0 Bytes)"
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 1, size: 0 Bytes)"
         )
         assert caplog.records[1].message == " > done."
         assert caplog.records[2].message == "\nWorkouts archives processed: 1."
@@ -255,7 +258,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
             cli,
             [
                 "workouts",
-                "archive_upload",
+                "archive_uploads",
                 "--max",
                 "1",
                 "--verbose",
@@ -265,7 +268,8 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         assert result.exit_code == 0
         assert (
             caplog.records[0].message
-            == "Processing task '1' (files: 2, size: 1.0 kB)"
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 2, size: 1.0 kB)"
         )
         assert caplog.records[1].message == " > starting archive processing..."
         assert caplog.records[2].message == "  - file 1/2"
@@ -304,7 +308,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
                 cli,
                 [
                     "workouts",
-                    "archive_upload",
+                    "archive_uploads",
                     "--max",
                     "1",
                 ],
@@ -318,7 +322,8 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         assert len(caplog.records) == 1
         assert (
             caplog.records[0].message
-            == "Processing task '1' (files: 1, size: 0 Bytes)"
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 1, size: 0 Bytes)"
         )
 
     def test_it_updates_task_on_error(
@@ -346,7 +351,7 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
                 cli,
                 [
                     "workouts",
-                    "archive_upload",
+                    "archive_uploads",
                     "--max",
                     "1",
                 ],
@@ -363,5 +368,159 @@ class TestCliWorkoutsArchiveUpload(UserTaskMixin):
         assert len(caplog.records) == 1
         assert (
             caplog.records[0].message
-            == "Processing task '1' (files: 1, size: 0 Bytes)"
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 1, size: 0 Bytes)"
         )
+
+
+class TestCliWorkoutsArchiveUploadTask(RandomMixin, UserTaskMixin):
+    def test_it_raises_error_when_process_workouts_archives_upload_raisies_exceptio(  # noqa
+        self, app: "Flask", caplog: "LogCaptureFixture", user_1: "User"
+    ) -> None:
+        runner = CliRunner()
+
+        result = runner.invoke(
+            cli,
+            [
+                "workouts",
+                "archive_upload",
+                "--id",
+                self.random_short_id(),
+            ],
+        )
+
+        assert result.exit_code == 1
+        assert result.exception is not None
+
+    def test_it_updates_task_on_error(
+        self,
+        app: "Flask",
+        caplog: "LogCaptureFixture",
+        user_1: "User",
+        sport_1_cycling: "Sport",
+    ) -> None:
+        upload_task = self.create_workouts_upload_task(
+            user_1,
+            workouts_data={"sport_id": sport_1_cycling.id},
+            files_to_process=["example.gpx"],
+            equipment_ids=None,
+            file_path="some path",
+        )
+        runner = CliRunner()
+
+        with patch.object(
+            WorkoutsFromArchiveCreationAsyncService,
+            "process",
+            side_effect=Exception(),
+        ):
+            result = runner.invoke(
+                cli,
+                [
+                    "workouts",
+                    "archive_upload",
+                    "--id",
+                    upload_task.short_id,
+                ],
+            )
+
+        db.session.refresh(upload_task)
+        assert upload_task.aborted is False
+        assert upload_task.errored is True
+        assert upload_task.errors == {
+            "archive": "error during archive processing",
+            "files": {},
+        }
+        assert result.exit_code == 1
+        assert len(caplog.records) == 1
+        assert (
+            caplog.records[0].message
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 1, size: 0 Bytes)"
+        )
+
+    def test_it_calls_process_workouts_archive_upload(
+        self,
+        app: "Flask",
+        user_1: "User",
+        sport_1_cycling: "Sport",
+        caplog: "LogCaptureFixture",
+    ) -> None:
+        upload_task = self.create_workouts_upload_task(
+            user_1,
+            workouts_data={"sport_id": sport_1_cycling.id},
+            files_to_process=["example.gpx"],
+            equipment_ids=None,
+            file_path="some path",
+        )
+        runner = CliRunner()
+
+        with patch(
+            "fittrackee.workouts.commands.process_workouts_archive_upload",
+        ) as process_workouts_archive_upload_mock:
+            result = runner.invoke(
+                cli,
+                [
+                    "workouts",
+                    "archive_upload",
+                    "--id",
+                    upload_task.short_id,
+                ],
+            )
+
+        process_workouts_archive_upload_mock.assert_called_once_with(
+            upload_task.short_id, logger
+        )
+
+        assert result.exit_code == 0
+        assert caplog.records[0].message == "\nDone."
+
+    def test_it_displays_message_when_verbose_is_enabled(
+        self,
+        app: "Flask",
+        caplog: "LogCaptureFixture",
+        user_1: "User",
+        sport_1_cycling: "Sport",
+    ) -> None:
+        fd, temp_file_path = tempfile.mkstemp(prefix="archive_", suffix=".zip")
+        with (
+            open(fd, "wb") as temp_file,
+            open(
+                os.path.join(
+                    app.root_path, "tests/files/gpx_test_incorrect.zip"
+                ),
+                "rb",
+            ) as zip_file,
+        ):
+            temp_file.write(zip_file.read())
+        upload_task = self.create_workouts_upload_task(
+            user_1,
+            workouts_data={"sport_id": sport_1_cycling.id},
+            files_to_process=["test_4.gpx", "test_1.gpx"],
+            equipment_ids=None,
+            file_path=temp_file_path,
+        )
+        upload_task.file_size = 1000
+        db.session.commit()
+        runner = CliRunner()
+
+        result = runner.invoke(
+            cli,
+            ["workouts", "archive_upload", "--id", upload_task.short_id, "-v"],
+        )
+
+        assert result.exit_code == 0
+        assert (
+            caplog.records[0].message
+            == f"Processing task '{upload_task.short_id}' "
+            f"(files: 2, size: 1.0 kB)"
+        )
+        assert caplog.records[1].message == " > starting archive processing..."
+        assert caplog.records[2].message == "  - file 1/2"
+        assert (
+            caplog.records[3].message
+            == "    > error occurred: no tracks in gpx file"
+        )
+        assert caplog.records[4].message == "  - file 2/2"
+        assert caplog.records[5].message == "    > upload done"
+        assert caplog.records[6].message == " > errored files: 1"
+        assert caplog.records[7].message == "\nDone."
--- fittrackee/workouts/tasks.py
+++ fittrackee/workouts/tasks.py
@@ -11,6 +11,7 @@ from fittrackee import db, dramatiq
 from fittrackee.constants import TASKS_TIME_LIMIT, TaskPriority
 from fittrackee.exceptions import TaskException
 from fittrackee.users.models import Notification, UserTask
+from fittrackee.utils import decode_short_id
 from fittrackee.workouts.services.workouts_from_file_creation_service import (
     WorkoutsFromArchiveCreationAsyncService,
 )
@@ -96,7 +97,33 @@ def upload_workouts_archive(task_id: int) -> None:
         db.session.close()
 
 
-def process_workouts_archives_upload(max_count: int, logger: Logger) -> int:
+def _handle_upload_task(upload_task: "UserTask", logger: "Logger") -> None:
+    files_count = len(upload_task.data.get("files_to_process", []))
+    file_size = str(upload_task.file_size) if upload_task.file_size else "0"
+    logger.info(
+        f"Processing task '{upload_task.short_id}' (files: {files_count}, "
+        f"size: {naturalsize(file_size)})"
+    )
+    try:
+        service = WorkoutsFromArchiveCreationAsyncService(upload_task.id)
+        service.process()
+        db.session.refresh(upload_task)
+        archive_error = upload_task.errors.get("archive")
+        if archive_error:
+            logger.info(f" > archive error: {archive_error}")
+        files_error = upload_task.errors.get("files", {})
+        if files_error:
+            logger.info(f" > errored files: {len(files_error.keys())}")
+    except (KeyboardInterrupt, Exception) as e:
+        db.session.rollback()
+        error = (
+            ABORT_ERROR if isinstance(e, KeyboardInterrupt) else GENERIC_ERROR
+        )
+        update_task_and_clean(error=error, upload_task_id=upload_task.id)
+        raise e
+
+
+def process_workouts_archives_uploads(max_count: int, logger: Logger) -> int:
     count = 0
     upload_tasks = (
         db.session.query(UserTask)
@@ -112,34 +139,25 @@ def process_workouts_archives_upload(max_count: int, logger: Logger) -> int:
     )
 
     for upload_task in upload_tasks:
-        files_count = len(upload_task.data.get("files_to_process", []))
-        file_size = (
-            str(upload_task.file_size) if upload_task.file_size else "0"
-        )
-        logger.info(
-            f"Processing task '{upload_task.id}' (files: {files_count}, "
-            f"size: {naturalsize(file_size)})"
-        )
-        try:
-            service = WorkoutsFromArchiveCreationAsyncService(upload_task.id)
-            service.process()
-            db.session.refresh(upload_task)
-            archive_error = upload_task.errors.get("archive")
-            if archive_error:
-                logger.info(f" > archive error: {archive_error}")
-            files_error = upload_task.errors.get("files", {})
-            if files_error:
-                logger.info(f" > errored files: {len(files_error.keys())}")
-            count += 1
-            logger.info(" > done.")
-        except (KeyboardInterrupt, Exception) as e:
-            db.session.rollback()
-            error = (
-                ABORT_ERROR
-                if isinstance(e, KeyboardInterrupt)
-                else GENERIC_ERROR
-            )
-            update_task_and_clean(error=error, upload_task_id=upload_task.id)
-            raise e
+        _handle_upload_task(upload_task, logger)
+        count += 1
+        logger.info(" > done.")
 
     return count
+
+
+def process_workouts_archive_upload(
+    task_short_id: str, logger: Logger
+) -> None:
+    try:
+        task_id = decode_short_id(task_short_id)
+    except Exception as e:
+        raise TaskException("Invalid task id") from e
+
+    upload_task = UserTask.query.filter_by(uuid=task_id).first()
+    if not upload_task:
+        raise TaskException("No task found")
+    if upload_task.progress > 0 or upload_task.errored or upload_task.aborted:
+        raise TaskException("Task is not queued")
+
+    _handle_upload_task(upload_task, logger)
