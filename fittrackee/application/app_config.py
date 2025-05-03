dc3c459cec21f2d7015e2c3c074addca7d3ce324
Sam <sam@samr1.net>
2025-05-03 17:18:10 +0200

    bump to 0.9.9

--- fittrackee/application/app_config.py
+++ fittrackee/application/app_config.py
@@ -56,7 +56,7 @@ def get_application_config() -> Union[Dict, HttpResponse]:
           "privacy_policy": null,
           "privacy_policy_date": null,
           "stats_workouts_limit": 10000,
-          "version": "0.9.8",
+          "version": "0.9.9",
           "weather_provider": null
         },
         "status": "success"
@@ -113,7 +113,7 @@ def update_application_config(auth_user: User) -> Union[Dict, HttpResponse]:
           "privacy_policy": null,
           "privacy_policy_date": null,
           "stats_workouts_limit": 10000,
-          "version": "0.9.8",
+          "version": "0.9.9",
           "weather_provider": null
         },
         "status": "success"
