eee2eeed155afff3c59a5caa5f0c5e9db0becbc2
Sam <sam@samr1.net>
2025-05-04 17:08:27 +0200

    API - init support for FIT files

--- fittrackee/workouts/services/workout_from_file/__init__.py
+++ fittrackee/workouts/services/workout_from_file/__init__.py
@@ -1,6 +1,7 @@
 from .base_workout_with_segment_service import (
     BaseWorkoutWithSegmentsCreationService,
 )
+from .workout_fit_creation_service import WorkoutFitCreationService
 from .workout_gpx_creation_service import GpxInfo, WorkoutGpxCreationService
 from .workout_kml_creation_service import WorkoutKmlCreationService
 from .workout_kmz_creation_service import WorkoutKmzCreationService
@@ -10,6 +11,7 @@ from .workout_tcx_creation_service import WorkoutTcxCreationService
 __all__ = [
     "BaseWorkoutWithSegmentsCreationService",
     "GpxInfo",
+    "WorkoutFitCreationService",
     "WorkoutGpxCreationService",
     "WorkoutKmlCreationService",
     "WorkoutKmzCreationService",
--- fittrackee_client/src/components/Workout/WorkoutEdition.vue
+++ fittrackee_client/src/components/Workout/WorkoutEdition.vue
@@ -66,7 +66,7 @@
                   id="gpxFile"
                   name="gpxFile"
                   type="file"
-                  accept=".gpx, .kml, .kmz, .tcx, .zip"
+                  accept=".gpx, .fit, .kml, .kmz, .tcx, .zip"
                   :disabled="loading"
                   required
                   @invalid="invalidateForm"
