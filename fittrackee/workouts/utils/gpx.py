6cc978854628b0ddd707075c9ed16a5030ea87b0
Sam <sam@samr1.net>
2025-06-28 18:03:44 +0200

    API - refactoring chart data by using dataframe

--- fittrackee/workouts/utils/gpx.py
+++ fittrackee/workouts/utils/gpx.py
@@ -1,13 +1,18 @@
 from datetime import timezone
-from typing import List, Optional
+from typing import TYPE_CHECKING, Dict, List, Optional
 
 import gpxpy.gpx
+import polars as pl
+from gpxpy.geo import distance as calculate_distance
 
 from ..constants import RPM_CADENCE_SPORTS, SPM_CADENCE_SPORTS
 from ..exceptions import WorkoutGPXException
 
+if TYPE_CHECKING:
+    from gpxpy.gpx import GPXTrackPoint
 
-def open_gpx_file(gpx_file: str) -> Optional[gpxpy.gpx.GPX]:
+
+def open_gpx_file(gpx_file: str) -> Optional["gpxpy.gpx.GPX"]:
     gpx_file = open(gpx_file, "r")  # type: ignore
     gpx = gpxpy.parse(gpx_file)
     if len(gpx.tracks) == 0:
@@ -36,6 +41,42 @@ def get_gpx_segments(
     return segments
 
 
+def calculate_distance_from_previous_point(struct: dict) -> float:
+    if struct["previous_latitude"] is None:
+        return 0.0
+    return calculate_distance(
+        struct["latitude"],
+        struct["longitude"],
+        struct["elevation"],
+        struct["previous_latitude"],
+        struct["previous_longitude"],
+        struct["previous_elevation"],
+    )
+
+
+def get_extensions(point: "GPXTrackPoint") -> Dict:
+    point_extensions: Dict = {"hr": None, "cadence": None}
+
+    if not point.extensions:
+        return point_extensions
+
+    extensions = []
+    for extension in point.extensions:
+        if "TrackPointExtension" in extension.tag:
+            extensions.extend(extension)
+        else:
+            extensions.append(extension)
+
+    for element in extensions:
+        if element.tag.endswith("hr") and element.text:
+            point_extensions["hr"] = int(element.text)
+        if (
+            element.tag.endswith("cad") or element.tag.endswith("cadence")
+        ) and element.text:
+            point_extensions["cadence"] = int(float(element.text))
+    return point_extensions
+
+
 def get_chart_data(
     gpx_file: str,
     sport_label: str,
@@ -58,87 +99,96 @@ def get_chart_data(
     if gpx is None:
         return None
 
-    chart_data = []
+    segments = get_gpx_segments(gpx.tracks[0].segments, segment_id)
+    if not segments:
+        return None
+
     first_point = None
-    previous_point = None
-    previous_distance = 0
+    for segment in segments:
+        if segment.points:
+            first_point = segment.points[0]
+            break
+    if not first_point:
+        return None
+
+    first_point_time = first_point.time.replace(
+        tzinfo=timezone(first_point.time.utcoffset())
+    )
+
     return_cadence = (
         workout_ave_cadence
         and sport_label in RPM_CADENCE_SPORTS + SPM_CADENCE_SPORTS
     )
     cadence_in_spm = sport_label in SPM_CADENCE_SPORTS
+    columns_to_return = [
+        "distance",
+        "duration",
+        "elevation",
+        "latitude",
+        "longitude",
+        "speed",
+        "time",
+    ]
+    if return_cadence:
+        columns_to_return.append("cadence")
+    if can_see_heart_rate:
+        columns_to_return.append("hr")
 
-    track_segments = gpx.tracks[0].segments
-    segments = get_gpx_segments(track_segments, segment_id)
-
-    for segment_idx, segment in enumerate(segments):
+    points = []
+    for segment in segments:
         for point_idx, point in enumerate(segment.points):
-            if segment_idx == 0 and point_idx == 0:
-                first_point = point
-            distance = (
-                point.distance_3d(previous_point)
-                if (
-                    point.elevation
-                    and previous_point
-                    and previous_point.elevation
-                )
-                else point.distance_2d(previous_point)
+            points.append(
+                {
+                    "longitude": point.longitude,
+                    "latitude": point.latitude,
+                    "elevation": point.elevation,
+                    "time": point.time.replace(
+                        tzinfo=timezone(point.time.utcoffset())
+                    ),
+                    "speed": segment.get_speed(point_idx),
+                    **get_extensions(point),
+                }
             )
-            distance = 0 if distance is None else distance
-            distance += previous_distance
-            speed = (
-                round((segment.get_speed(point_idx) / 1000) * 3600, 2)
-                if segment.get_speed(point_idx) is not None
-                else 0
-            )
-            data = {
-                "distance": (
-                    round(distance / 1000, 2) if distance is not None else 0
-                ),
-                "duration": int(point.time_difference(first_point)),
-                "latitude": point.latitude,
-                "longitude": point.longitude,
-                "speed": speed,
-                # workaround
-                # https://github.com/tkrajina/gpxpy/issues/209
-                "time": point.time.replace(
-                    tzinfo=timezone(point.time.utcoffset())
-                ),
-            }
-            if point.elevation:
-                data["elevation"] = round(point.elevation, 1)
-            if point.extensions:
-                extensions = []
-                for extension in point.extensions:
-                    if "TrackPointExtension" in extension.tag:
-                        extensions.extend(extension)
-                    else:
-                        extensions.append(extension)
-                for element in extensions:
-                    if (
-                        can_see_heart_rate
-                        and element.tag.endswith("hr")
-                        and element.text
-                    ):
-                        data["hr"] = int(element.text)
-                    if (
-                        return_cadence
-                        and (
-                            element.tag.endswith("cad")
-                            or element.tag.endswith("cadence")
-                        )
-                        and element.text
-                    ):
-                        data["cadence"] = (
-                            int(float(element.text)) * 2
-                            if cadence_in_spm
-                            else int(float(element.text))
-                        )
-            chart_data.append(data)
-            previous_point = point
-            previous_distance = distance
 
-    return chart_data
+    df = (
+        pl.LazyFrame(
+            data=points,
+        )
+        .with_columns(
+            duration=(pl.col("time") - first_point_time).dt.total_seconds()
+        )
+        .with_columns(previous_longitude=pl.col("longitude").shift(1))
+        .with_columns(previous_latitude=pl.col("latitude").shift(1))
+        .with_columns(previous_elevation=pl.col("elevation").shift(1))
+        .with_columns(
+            point_distance=pl.struct(
+                pl.col("latitude"),
+                pl.col("longitude"),
+                pl.col("elevation"),
+                pl.col("previous_latitude"),
+                pl.col("previous_longitude"),
+                pl.col("previous_elevation"),
+            ).map_elements(
+                calculate_distance_from_previous_point, return_dtype=pl.Float64
+            )
+        )
+        .with_columns(
+            distance=(pl.col("point_distance").cum_sum() / 1000).round(2)
+        )
+        .with_columns(speed=(pl.col("speed") / 1000 * 3600).round(2))
+        .with_columns(
+            cadence=pl.when(cadence_in_spm)
+            .then(pl.col("cadence") * 2)
+            .otherwise(pl.col("cadence"))
+        )
+    )
+
+    return (
+        df.select(*columns_to_return)
+        .drop(pl.selectors.by_dtype(pl.Null))
+        .collect()
+        .rows(named=True)
+    )
 
 
 def extract_segment_from_gpx_file(
