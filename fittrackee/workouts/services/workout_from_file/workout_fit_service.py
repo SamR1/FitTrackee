90b1c6869e02b0e7d2d573511d1c48544ec54b98
Sam <sam@samr1.net>
2026-04-04 18:29:30 +0200

    API - minor refactoring

--- fittrackee/workouts/services/workout_from_file/workout_fit_service.py
+++ fittrackee/workouts/services/workout_from_file/workout_fit_service.py
@@ -1,4 +1,4 @@
-from typing import IO, Optional
+from typing import IO, TYPE_CHECKING, Any, List, Optional
 
 import fitdecode
 import gpxpy.gpx
@@ -8,6 +8,9 @@ from ...exceptions import WorkoutFileException
 from .constants import GARMIN_DEVICES
 from .workout_gpx_service import WorkoutGpxService
 
+if TYPE_CHECKING:
+    from fitdecode.records import FitDataMessage
+
 
 class WorkoutFitService(WorkoutGpxService):
     @staticmethod
@@ -16,6 +19,59 @@ class WorkoutFitService(WorkoutGpxService):
         converts coordinates from semicircles
         """
         return value * (180.0 / 2**31)
+
+    @staticmethod
+    def get_creator(data_frames: List["FitDataMessage"]) -> Optional[str]:
+        creator = None
+        # Handle device metadata from file_id
+        file_id_frames = filter(lambda f: f.name == "file_id", data_frames)
+        frame = next(file_id_frames, None)
+        if not frame:
+            return creator
+
+        if frame.has_field("product_name"):
+            creator = frame.get_value("product_name")
+            if isinstance(creator, str):
+                creator = creator.capitalize()
+        elif frame.has_field("manufacturer"):
+            creator = (
+                frame.get_value("manufacturer")
+                if isinstance(frame.get_value("manufacturer"), str)
+                else None
+            )
+            if (
+                creator
+                and frame.has_field("product")
+                and frame.get_value("product")
+            ):
+                product = frame.get_raw_value("product")
+                if (
+                    creator.lower() == "garmin"
+                    and product in GARMIN_DEVICES.keys()
+                ):
+                    product = GARMIN_DEVICES[product]
+                creator = f"{creator} {product}"
+        return creator
+
+    @staticmethod
+    def get_total_calories(
+        data_frames: List["FitDataMessage"],
+    ) -> Optional[str]:
+        # Get total calories from session
+        # - total calories = resting + active calories
+        # - units: kcal
+        calories = None
+        session_frames = filter(lambda f: f.name == "session", data_frames)
+        frame = next(session_frames, None)
+        if frame and frame.has_field("total_calories"):
+            calories = frame.get_value("total_calories")
+        return calories
+
+    @staticmethod
+    def get_value_from_frame(frame: "FitDataMessage", key: str) -> Any:
+        if frame.has_field(key):
+            return frame.get_value(key)
+        return None
 
     @classmethod
     def parse_file(
@@ -46,8 +102,6 @@ class WorkoutFitService(WorkoutGpxService):
 
         gpx_track = gpxpy.gpx.GPXTrack()
         gpx_segment = gpxpy.gpx.GPXTrackSegment()
-        creator: Optional[str] = None
-        calories: Optional[str] = None
         try:
             data_frames = list(
                 filter(
@@ -56,44 +110,9 @@ class WorkoutFitService(WorkoutGpxService):
                 )
             )
 
-            # Handle device metadata from file_id
-            file_id_frames = filter(
-                lambda frame: frame.name == "file_id", data_frames
-            )
-            frame = next(file_id_frames, None)
-            if frame:
-                if frame.has_field("product_name"):
-                    creator = frame.get_value("product_name")
-                    if isinstance(creator, str):
-                        creator = creator.capitalize()
-                elif frame.has_field("manufacturer"):
-                    creator = (
-                        frame.get_value("manufacturer")
-                        if isinstance(frame.get_value("manufacturer"), str)
-                        else None
-                    )
-                    if (
-                        creator
-                        and frame.has_field("product")
-                        and frame.get_value("product")
-                    ):
-                        product = frame.get_raw_value("product")
-                        if (
-                            creator.lower() == "garmin"
-                            and product in GARMIN_DEVICES.keys()
-                        ):
-                            product = GARMIN_DEVICES[product]
-                        creator = f"{creator} {product}"
+            creator = cls.get_creator(data_frames)
 
-            # Get total calories from session
-            # - total calories = resting + active calories
-            # - units: kcal
-            session_frames = filter(
-                lambda frame: frame.name == "session", data_frames
-            )
-            frame = next(session_frames, None)
-            if frame and frame.has_field("total_calories"):
-                calories = frame.get_value("total_calories")
+            calories = cls.get_total_calories(data_frames)
 
             # Handle the actual data frames. We sort them by timestamp
             # to handle devices that list events and records separately.
@@ -131,49 +150,23 @@ class WorkoutFitService(WorkoutGpxService):
                 if frame.name != "record":
                     continue
 
-                longitude = (
-                    frame.get_value("position_long")
-                    if frame.has_field("position_long")
-                    else None
-                )
-                latitude = (
-                    frame.get_value("position_lat")
-                    if frame.has_field("position_lat")
-                    else None
-                )
-                time = (
-                    frame.get_value("timestamp")
-                    if frame.has_field("timestamp")
-                    else None
-                )
+                longitude = cls.get_value_from_frame(frame, "position_long")
+                latitude = cls.get_value_from_frame(frame, "position_lat")
+                time = cls.get_value_from_frame(frame, "timestamp")
                 if not longitude or not latitude or not time:
                     continue
 
-                elevation = (
-                    frame.get_value("enhanced_altitude")
-                    if frame.has_field("enhanced_altitude")
-                    else None
+                elevation = cls.get_value_from_frame(
+                    frame, "enhanced_altitude"
                 )
                 # some devices store elevation as a tuple instead of a float
                 if isinstance(elevation, tuple):
                     elevation = (
                         elevation[0] if elevation[0] is not None else None
                     )
-                heart_rate = (
-                    frame.get_value("heart_rate")
-                    if frame.has_field("heart_rate")
-                    else None
-                )
-                cadence = (
-                    frame.get_value("cadence")
-                    if frame.has_field("cadence")
-                    else None
-                )
-                power = (
-                    frame.get_value("power")
-                    if frame.has_field("power")
-                    else None
-                )
+                heart_rate = cls.get_value_from_frame(frame, "heart_rate")
+                cadence = cls.get_value_from_frame(frame, "cadence")
+                power = cls.get_value_from_frame(frame, "power")
 
                 point = gpxpy.gpx.GPXTrackPoint(
                     longitude=cls.get_coordinate(longitude),
