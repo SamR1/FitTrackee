e53f75e4c3bdd31f918d02ef5dde5eadbfdb58fa
Sam <sam@samr1.net>
2026-07-13 16:59:41 +0200

    API - reformat test file

--- fittrackee/tests/workouts/test_workouts_map_tiles_api.py
+++ fittrackee/tests/workouts/test_workouts_map_tiles_api.py
@@ -27,10 +27,7 @@ class TestGetTileResponseHeaders:
             == "max-age=86400, stale-while-revalidate=604800"
         )
         assert response_headers["ETag"] == '"tile-etag"'
-        assert (
-            response_headers["Expires"]
-            == "Wed, 24 Jun 2026 16:46:03 GMT"
-        )
+        assert response_headers["Expires"] == "Wed, 24 Jun 2026 16:46:03 GMT"
         assert (
             response_headers["Last-Modified"]
             == "Tue, 23 Jun 2026 16:46:03 GMT"
